1a63b043e5668ac18c9bfcd99d27aba5cdfe88c3
Sushan <t2masu00@students.oamk.fi>
2023-11-14 21:52:49 +0200

    completed basic frontend for the signup and login page

--- parcel/src/App.js
+++ parcel/src/App.js
@@ -1,18 +1,28 @@
-import "./App.css";
-// import Navbar from "./Components/Navbar.jsx";
-// import Banner from "./Components/Banner.jsx";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home.jsx";
-import { BrowserRouter } from "react-router-dom";
+import Login from "./Components/Login";
+import SignUpPage from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
+
+function Layout() {
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
+}
 
 function App() {
 	return (
-		<div className="App">
-			<BrowserRouter>
-				{/* <Navbar />
-				<Banner /> */}
-				<Home />
-			</BrowserRouter>
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Layout />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</Router>
 	);
 }
 
---
+++ parcel/src/Components/Signup.jsx
@@ -0,0 +1,80 @@
+// src/SignUpPage.js
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import "./Signup.css";
+
+const SignUpPage = () => {
+	const navigate = useNavigate(); // Initialize useNavigate
+
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSignUp = () => {
+		// Handle signup logic (submit data to backend, perform validation, etc.)
+		console.log("Signup data:", { firstName, lastName, email, password });
+
+		// Navigate back to the sign-in page after signup
+		navigate("/login");
+	};
+
+	return (
+		<div className="main-container">
+			<div className="signup-container">
+				<h2>Create an Account</h2>
+				<div>
+					<label htmlFor="firstName">First Name:</label>
+					<input
+						type="text"
+						id="firstName"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="lastName">Last Name:</label>
+					<input
+						type="text"
+						id="lastName"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="phone">Phone Number:</label>
+					<input
+						type="text"
+						id="number"
+						value={phone}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<div>
+					<button onClick={handleSignUp}>Sign Up</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SignUpPage;
